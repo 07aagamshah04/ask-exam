f752581b5407a487ab85bca2989ee4c1f2ad0c1a
07aagamshah04 <155041336+07aagamshah04@users.noreply.github.com>
2025-10-30 09:43:33 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 # Google Gemini
 from google import genai
-genai_client = genai.Client(api_key="AIzaSyDOisJFtcZNIfGQPGQ1XiCO_uJ6i4TretI")
+genai_client = genai.Client(api_key="AIzaSyBJLTMti0j86A6kWwiGDLH4GX8ZglrMlfU")
 
 # Groq models
 from groq import Groq
